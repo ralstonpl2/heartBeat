e555d08eff7feb4ee3e03862e85edb0c5f8acca6
luisa <Luisa@Luisas-MacBook-Pro.local>
2016-08-03 16:52:22 -0700

    working on familyview

--- client/familyView/family.js
+++ client/familyView/family.js
@@ -1,8 +1,12 @@
 app = angular.module('heartbeat.family', []);
 
-app.controller('familyCtrl', function($scope, familyFactory){
-  $scope.names = {}
+app.controller('familyCtrl', function($scope, $window, familyFactory){
+  $scope.names = {};
 
+  $scope.getNames = function(){
+    console.log('hi')
+    var familyObj = familyFactory.getFamilyNames($window.localStorage.getItem('heartbeat'))
+  }
 
   //call the get function and push the names into '$scope.names'
 
@@ -10,6 +14,21 @@ app.controller('familyCtrl', function($scope, familyFactory){
     // if the selected person in the same as the current change selected person to everyone
 })
 
-app.factory('familyFactory', function(){
+app.factory('familyFactory', function($http){
 // create a get function that gets all the names on the database 
+  var getFamilyNames = function(id){
+
+    return $http({
+      type: "GET",
+      url: '/api/family/' + id
+    }).then(function(res){
+      console.log(res)
+      return res
+    })
+  }
+
+  return {
+    getFamilyNames: getFamilyNames
+  }
+
 })
